1f903a5770a5feaee4dc4338b9e6c0c0773fb967
famid <ahsanulhoque721@gmail.com>
2020-07-22 11:38:59 +0600

    Update | users_table username nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('username')->unique();
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->tinyInteger('role')->default(USER_ROLE);
