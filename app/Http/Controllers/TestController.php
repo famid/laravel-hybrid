8b1e481ddf1b21d3e3103bfc870de3ba753580f6
famid <ahsanulhoque721@gmail.com>
2020-07-26 01:57:33 +0600

    Add | login api Modified| login service

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,18 +4,24 @@
 namespace App\Http\Controllers;
 
 
+use Symfony\Component\HttpFoundation\Request;
+
 class TestController extends Controller {
 
     public function test() {
         try {
-             return $this->webResponse(false, "Failed");
+            $b = 'http://localhost:8000/test-redirect/'.encrypt('fuck you fucker');
+
+            $a = '<a href = "'.$b.'">' .$b. '</a>';
+
+             return $a;
 //            return $this->webResponse(true, "Success", 'test.redirect');
         } catch (\Exception $e) {
             return $this->webResponse(false, $e->getMessage());
         }
     }
 
-    public function testRedirect() {
-        return "OK";
+    public function testRedirect($id) {
+        return decrypt($id);
     }
 }
--- config/services.php
+++ config/services.php
@@ -41,5 +41,11 @@ return [
         'client_secret' => env('GITHUB_SECRET'),
         'redirect' => env('GITHUB_URL'),
     ],
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT'),
+    ],
+
 
 ];
