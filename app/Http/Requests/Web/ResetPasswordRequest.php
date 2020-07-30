d02ecf219c823f6e0fb92914050af3fd47070ac6
famid <ahsanulhoque721@gmail.com>
2020-07-30 11:50:15 +0600

    Add | email validator field

--- app/Http/Requests/Web/ResetPasswordRequest.php
+++ app/Http/Requests/Web/ResetPasswordRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Web;
 
+use App\Rules\EmailOrUsernameValidation;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ResetPasswordRequest extends FormRequest
@@ -24,6 +25,7 @@ class ResetPasswordRequest extends FormRequest
     public function rules()
     {
         return [
+            'email' =>  ['required', 'string', 'max:255', new EmailOrUsernameValidation()],
             'reset_password_code' => 'required',
             'new_password' => 'required|min:8',
             'confirm_password' => 'required|same:new_password'
@@ -41,6 +43,9 @@ class ResetPasswordRequest extends FormRequest
             'new_password.min' => __('New password must be al least 8 characters'),
             'confirm_password.required' => __('Confirm password can not be empty'),
             'confirm_password.same' => __('New password and confirm password are not same'),
+            'email.required' => __('Invalid email or reset code'),
+            'email.string' => __('Invalid email or reset code'),
+            'email.max:255' => __('Invalid email or reset code')
         ];
     }
 }
