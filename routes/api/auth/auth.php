072029a882ad21ed217cfb241306494533b66a4b
famid <ahsanulhoque721@gmail.com>
2020-07-30 12:36:51 +0600

    Add | added resend email verification code endpoint Fix | the middleware error

--- routes/api/auth/auth.php
+++ routes/api/auth/auth.php
@@ -11,12 +11,14 @@ Route::group(['namespace' => 'Api\Auth'] , function () {
     Route::post('sign-up-process', "RegisterController@signUpProcess")->name('api.auth.sign_up_process');
     Route::post('sign-in-process', "LoginController@signInProcess")->name('api.auth.sign_in_process');
     Route::post('verify-email-process', "VerificationController@verifyEmailProcess")->name('api.auth.verify_email_process');
+    Route::post('resend-email-verification-code', "VerificationController@resendEmailVerificationCode")
+        ->name('api.auth.resend_email_verification_code');
     Route::post('forget-password-email-send', "ForgotPasswordController@forgetPasswordEmailSendProcess")
         ->name('api.auth.forget_password_email_send_process');
     Route::post('reset-password-process', "ResetPasswordController@resetPasswordProcess")
         ->name('api.auth.reset_password_process');
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('sign-out', 'Api\Auth\LoginController@signOut')->name('api.auth.sign_out');
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('sign-out', 'Api\Auth\LoginController@signOut')->name('api.auth.sign_out');
 });
