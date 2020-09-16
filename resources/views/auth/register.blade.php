950ed98a405acb35b487bf5620fdd4d1d08ebe58
famid <ahsanulhoque721@gmail.com>
2020-09-16 22:17:38 +0600

    Modified | refactor register.blade.php code and correct some input filed type

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,25 +7,27 @@
             background-image: url({{asset('images/images.jpeg') }});
         ">
         <div class="row">
-            <div class="col-md-12 text-center " {{--style="background-size: cover;background-image: url(./assets/images/photo-long-3.jpg)"--}}>
+            <div class="col-md-12 text-center ">
                 <div class="p-4 auth-right">
                     <div class="auth-logo text-center mt-4">
                         <img src="{{asset("assets/images/logo.png")}}" alt="logo">
                     </div>
                     <div class="flex-grow-1"></div>
                     <div class="w-100 mb-4">
-                        <a class="btn btn-outline-google btn-block btn-icon-text btn-rounded" href="{{route
-                    ('web.auth.redirect_to_provider', ['google'])}}">
-                            <i class="i-Google-Plus"></i> Sign Up with Google
+                        <a class="btn btn-outline-google btn-block btn-icon-text btn-rounded"
+                           href="{{route('web.auth.redirect_to_provider', ['google'])}}">
+                            <i class="i-Google-Plus"></i>
+                            Sign Up with Google
                         </a>
-                        <a class="btn btn-outline-facebook btn-block btn-icon-text btn-rounded" href="{{route
-                    ('web.auth.redirect_to_provider', ['facebook'])}}">
-                            <i class="i-Facebook-2"></i> Sign Up with Facebook
+                        <a class="btn btn-outline-facebook btn-block btn-icon-text btn-rounded"
+                           href="{{route('web.auth.redirect_to_provider', ['facebook'])}}">
+                            <i class="i-Facebook-2"></i>
+                            Sign Up with Facebook
                         </a>
-                        <a class="btn btn-outline-success btn-block btn-icon-text btn-rounded" href="{{route
-                    ('web.auth.redirect_to_provider',
-                    ['github'])}}">
-                            <i class="fa fa-github" aria-hidden="true"></i> Login with GitHub
+                        <a class="btn btn-outline-success btn-block btn-icon-text btn-rounded"
+                           href="{{route('web.auth.redirect_to_provider',['github'])}}">
+                            <i class="fa fa-github" aria-hidden="true"></i>
+                            Login with GitHub
                         </a>
                     </div>
                     <div class="flex-grow-1"></div>
@@ -34,83 +36,81 @@
 
             <div class="col-md-12">
                 <div class="p-4">
-
                     <h1 class="mb-3 text-18">Sign Up</h1>
                     <form method="POST" action="{{ route('web.auth.sign_up_process') }}">
                         @csrf
                         <div class="form-group">
                             <label for="first-name">First name</label>
-                            <input id="firstName" type="text" class="form-control form-control-rounded @error('first_name')
-                                    is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
+                            <input id="firstName" type="text" name="first_name" class="form-control form-control-rounded  @error('first_name') is-invalid @enderror"
+                                   value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
 
                             @error('first_name')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label for="last-name">Last name</label>
-                            <input id="lastName" type="text" class="form-control form-control-rounded @error('last_name')
-                                    is-invalid
-@enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                            <input id="lastName" type="text" name="last_name" class="form-control form-control-rounded @error('last_name') is-invalid @enderror"
+                                   value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
 
                             @error('last_name')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input id="username" type="text" class="form-control form-control-rounded @error('username')
-                                    is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                            <input id="username" type="text" name="username" class="form-control form-control-rounded @error('username') is-invalid @enderror"
+                                   value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                             @error('username')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label for="email">Email address</label>
-                            <input id="email" type="email" class="form-control form-control-rounded @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <input id="email" type="email" name="email" class="form-control form-control-rounded @error('email') is-invalid @enderror"
+                                   value="{{ old('email') }}" required autocomplete="email">
 
                             @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
-                            <label for="phone_code">Select</label>
-                            <label>
-                                <select name="phone_code" class="form-control form-control-rounded">
-                                    <option value="+880">+880</option>
-                                    <option value="+881">+881</option>
-                                    <option value="+882">+882</option>
-                                </select>
-                            </label>
+                            <label for="phone_code">Select Your Phone Code</label>
+                            <select name="phone_code" class="form-control form-control-rounded">
+                                <option value="+880">+880</option>
+                                <option value="+881">+881</option>
+                                <option value="+882">+882</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label for="phone">Phone</label>
-                            <input id="phone" type="number" class="form-control form-control-rounded @error('phone')
-                                    is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
+                            <input id="phone" type="tel" name="phone" class="form-control form-control-rounded @error('phone')is-invalid @enderror"
+                                   value="{{ old('phone') }}" required autocomplete="phone">
 
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                            @error('phone')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input id="password" type="password" class="form-control form-control-rounded @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <input id="password" type="password" name="password"  class="form-control form-control-rounded @error('password') is-invalid @enderror"
+                                   required autocomplete="new-password">
 
                             @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                             @enderror
                         </div>
                         <div class="form-group">
