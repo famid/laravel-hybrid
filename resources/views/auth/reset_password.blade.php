009aaa71142d393c4b993fefddda904ad51fe6a4
famid <ahsanulhoque721@gmail.com>
2020-09-17 11:02:58 +0600

    Modified | refactor code Fix | the path of logo image

--- resources/views/auth/reset_password.blade.php
+++ resources/views/auth/reset_password.blade.php
@@ -1,17 +1,15 @@
 @extends('auth.layouts.auth')
 
 @section('content')
-    <body class="text-left">
-    <div class="auth-layout-wrap" style="
-            background-image: url({{asset('images/images.jpeg') }});">
+    <div class="auth-layout-wrap" style="background-image: url({{asset('images/images.jpeg') }});">
         <div class="auth-content">
             <div class="row">
                     <div class="col-md-6">
                         <div class="p-4">
                             <div class="auth-logo text-center mb-4">
-                                <img src="" alt="">
+                                <img src="{{asset('assets/images/logo.png')}}" alt="">
                             </div>
-                            <h1 class="mb-3 text-18">Forgot Password</h1>
+                            <h1 class="mb-3 text-18">{{__('Reset Your Password')}}</h1>
                             <form method="POST" action="{{ route('web.auth.reset_password_process') }}">
                                 @csrf
                                 @if(isset($email))
@@ -25,8 +23,8 @@
                                 <div class="form-group">
                                     <label for="reset_password_code">{{__('Reset Password Code')}}</label>
                                     <input name="reset_password_code" id="reset_password_code"
-                                           class="form-control form-control-rounded" type="number" value="{{ old
-                                           ('reset_password_code') }}">
+                                           class="form-control form-control-rounded" type="number"
+                                           value="{{ old('reset_password_code') }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="password">{{__('New Password')}}</label>
