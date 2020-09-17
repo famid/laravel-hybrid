02899bb6ebaa6628cfb92a67bd2f2328a4cd955d
famid <ahsanulhoque721@gmail.com>
2020-09-17 11:21:24 +0600

    Modified | verify-email route parameter name and refactor indentation Remove | email-verification route

--- routes/web/auth/auth.php
+++ routes/web/auth/auth.php
@@ -15,15 +15,16 @@ Route::group(['namespace' => 'Web\Auth'] , function () {
     Route::get('sign-in', "LoginController@signIn")->name('web.auth.sign_in');
     Route::post('sign-in-process', "LoginController@signInProcess")->name('web.auth.sign_in_process');
 
-//    Route::get('email-verification', "VerificationController@emailVerificationView")->name('web.auth.email_verification');
-    Route::get('verify-email/{token}',"VerificationController@verifyEmailProcess")
+    Route::get('verify-email/{encryptUserId}',"VerificationController@verifyEmailProcess")
         ->name('web.auth.verify_email_process');
 
-    Route::get('forget-password', "ForgotPasswordController@forgetPassword")->name('web.auth.forget_password');
+    Route::get('forget-password', "ForgotPasswordController@forgetPassword")
+        ->name('web.auth.forget_password');
     Route::post('forget-password-email-send', "ForgotPasswordController@forgetPasswordEmailSendProcess")
         ->name('web.auth.forget_password_email_send_process');
 
-    Route::get('reset-password/{email}', "ResetPasswordController@resetPassword")->name('web.auth.reset_password');
+    Route::get('reset-password/{email}', "ResetPasswordController@resetPassword")
+        ->name('web.auth.reset_password');
     Route::post('reset-password-process', "ResetPasswordController@resetPasswordProcess")
         ->name('web.auth.reset_password_process');
 
