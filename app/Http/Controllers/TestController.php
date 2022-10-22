184b0bd009ca70789ba4e0d299b3949540d63b20
famid <ahsanulhoquefamid@gmail.com>
2022-10-22 17:17:27 +0600

    Remove | test code from TestController class

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,26 +8,4 @@ use Symfony\Component\HttpFoundation\Request;
 
 class TestController extends Controller {
 
-    public function test() {
-      return view('auth.forget_password_email');
-    }
-
-    public function testRedirect(Request $request) {
-        $data = [
-            'success' => true,
-            "message" => "OK",
-            "data" => [
-                "id" => 1,
-                "name" => "Lewra leson",
-                "lewra" => "leson"
-            ]
-        ];
-        return $this->webResponse(
-            $data,
-            'success.route',
-            null,
-            ['id' => $data["data"]["id"]],
-            ['id' => $request->id]
-        );
-    }
 }
